90d996b5091d8aaf93e1c32872efde0aacf1763d
martin v <villasen@unlv.nevada.edu>
2020-01-04 11:32:32 -0800

    add libraries to models.py

--- speech_commands/models.py
+++ speech_commands/models.py
@@ -22,6 +22,14 @@ from __future__ import print_function
 import math
 
 import tensorflow as tf
+import tensorflow.contrib.slim as slim
+from tensorflow.contrib.layers.python.layers import layers
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import init_ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import nn_ops
+from tensorflow.python.ops import rnn_cell_impl
+from tensorflow.python.ops import variable_scope as vs
 
 
 def _next_power_of_two(x):
