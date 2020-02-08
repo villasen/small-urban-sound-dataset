c7e3a099a1de606f526bcb00143f514f7ece4f3c
martin v <villasen@unlv.nevada.edu>
2020-02-07 22:00:55 -0800

    Update models.py

--- speech_commands/models.py
+++ speech_commands/models.py
@@ -155,6 +155,11 @@ def create_model(fingerprint_input, model_settings, model_architecture,
   elif model_architecture == 'martin_urban_model':
     return create_martin_urban_conv_model(fingerprint_input, model_settings,
                                           is_training)
+
+  elif model_architecture == 'martin_tiny_urban_model':
+    return create_martin_tiny_conv_model(fingerprint_input, model_settings,
+                                          is_training)
+
   elif model_architecture == 'ds_cnn':
       return create_ds_cnn_model(fingerprint_input, model_settings,
                                   model_size_info, is_training)
@@ -731,9 +736,9 @@ def create_tiny_conv_model(fingerprint_input, model_settings, is_training):
   input_time_size = model_settings['spectrogram_length']
   fingerprint_4d = tf.reshape(fingerprint_input,
                               [-1, input_time_size, input_frequency_size, 1])
-  first_filter_width = 8
-  first_filter_height = 10
-  first_filter_count = 8
+  first_filter_width = 8  #time
+  first_filter_height = 10 #frequency
+  first_filter_count = 8  # channels
   first_weights = tf.compat.v1.get_variable(
       name='first_weights',
       initializer=tf.compat.v1.truncated_normal_initializer(stddev=0.01),
@@ -874,8 +879,10 @@ def create_tiny_embedding_conv_model(fingerprint_input, model_settings,
       name='second_bias',
       initializer=tf.compat.v1.zeros_initializer,
       shape=[second_filter_count])
-  second_conv_stride_x = 8
-  second_conv_stride_y = 8
+#  second_conv_stride_x = 8
+#  second_conv_stride_y = 8
+  second_conv_stride_x = 1
+  second_conv_stride_y = 1
   second_conv = tf.nn.conv2d(
       input=first_dropout, filters=second_weights,
       strides=[1, second_conv_stride_y, second_conv_stride_x, 1],
@@ -972,6 +979,8 @@ def create_martin_urban_conv_model(fingerprint_input, model_settings,
   fingerprint_4d = tf.reshape(fingerprint_input,
                               [-1, input_time_size, input_frequency_size, 1])
 
+
+# First layer
   first_filter_width = 8
   first_filter_height = 10
   first_filter_count = 8
@@ -995,6 +1004,32 @@ def create_martin_urban_conv_model(fingerprint_input, model_settings,
   else:
     first_dropout = first_relu
 
+
+
+
+tf.nn.separable_conv2d(
+    input,
+    depthwise_filter,
+    pointwise_filter,
+    strides,
+    padding,
+    rate=None,
+    name=None,
+    data_format=None,
+    dilations=None
+)
+
+tf.nn.conv2d(
+    input,
+    filters,
+    strides,
+    padding,
+    data_format='NHWC',
+    dilations=None,
+    name=None
+)
+
+# Second layer
   second_filter_width = 8
   second_filter_height = 10
   second_filter_count = 8
@@ -1011,10 +1046,20 @@ def create_martin_urban_conv_model(fingerprint_input, model_settings,
       shape=[second_filter_count])
   second_conv_stride_x = 8
   second_conv_stride_y = 8
-  second_conv = tf.nn.conv2d(
-      input=first_dropout, filters=second_weights,
+  # skip pointwise by setting filters=None
+  second_conv = tf.nn.separable_conv2d(
+      input=first_dropout, filters=None,
       strides=[1, second_conv_stride_y, second_conv_stride_x, 1],
       padding='SAME') + second_bias
+  
+      pointwise_conv = tf.nn.convolution2d(second_conv,
+                                        first_weights,
+                                       kernel_size=[1, 1],
+                                        scope=sc+'/pw_conv')
+
+
+  pointwise_conv = tf.nn.convolution2d(second_conv, )
+
   second_relu = tf.nn.relu(second_conv)
   if is_training:
     second_dropout = tf.compat.v1.nn.dropout(second_relu, dropout_prob)
@@ -1046,6 +1091,100 @@ def create_martin_urban_conv_model(fingerprint_input, model_settings,
     return final_fc
 
 
+def create_martin_tiny_conv_model(fingerprint_input, model_settings, is_training):
+  """Builds a convolutional model aimed at microcontrollers.
+
+  Devices like DSPs and microcontrollers can have very small amounts of
+  memory and limited processing power. This model is designed to use less
+  than 20KB of working RAM, and fit within 32KB of read-only (flash) memory.
+
+  Here's the layout of the graph:
+
+  (fingerprint_input)
+          v
+      [Conv2D]<-(weights)
+          v
+      [BiasAdd]<-(bias)
+          v
+        [Relu]
+          v
+      [MatMul]<-(weights)
+          v
+      [BiasAdd]<-(bias)
+          v
+
+  This doesn't produce particularly accurate results, but it's designed to be
+  used as the first stage of a pipeline, running on a low-energy piece of
+  hardware that can always be on, and then wake higher-power chips when a
+  possible utterance has been found, so that more accurate analysis can be done.
+
+  During training, a dropout node is introduced after the relu, controlled by a
+  placeholder.
+
+  Args:
+    fingerprint_input: TensorFlow node that will output audio feature vectors.
+    model_settings: Dictionary of information about the model.
+    is_training: Whether the model is going to be used for training.
+
+  Returns:
+    TensorFlow node outputting logits results, and optionally a dropout
+    placeholder.
+  """
+  
+  # First layer
+  if is_training:
+    dropout_prob = tf.compat.v1.placeholder(tf.float32, name='dropout_prob')
+  input_frequency_size = model_settings['fingerprint_width']
+  input_time_size = model_settings['spectrogram_length']
+  fingerprint_4d = tf.reshape(fingerprint_input,
+                              [-1, input_time_size, input_frequency_size, 1])
+  first_filter_width = 8  #time
+  first_filter_height = 10 #frequency
+  first_filter_count = 8  # channels
+  first_weights = tf.compat.v1.get_variable(
+      name='first_weights',
+      initializer=tf.compat.v1.truncated_normal_initializer(stddev=0.01),
+      shape=[first_filter_height, first_filter_width, 1, first_filter_count])
+  first_bias = tf.compat.v1.get_variable(
+      name='first_bias',
+      initializer=tf.compat.v1.zeros_initializer,
+      shape=[first_filter_count])
+  first_conv_stride_x = 2
+  first_conv_stride_y = 2
+  first_conv = tf.nn.conv2d(   #   Conv2d layer
+      input=fingerprint_4d, filters=first_weights,
+      strides=[1, first_conv_stride_y, first_conv_stride_x, 1],
+      padding='SAME') + first_bias
+  first_relu = tf.nn.relu(first_conv)
+  
+  # Second layer
+  if is_training:
+    first_dropout = tf.compat.v1.nn.dropout(first_relu, dropout_prob)
+  else:
+    first_dropout = first_relu
+  first_dropout_shape = first_dropout.get_shape()
+  first_dropout_output_width = first_dropout_shape[2]
+  first_dropout_output_height = first_dropout_shape[1]
+  first_dropout_element_count = int(
+      first_dropout_output_width * first_dropout_output_height *
+      first_filter_count)
+  flattened_first_dropout = tf.reshape(first_dropout,
+                                       [-1, first_dropout_element_count])
+  label_count = model_settings['label_count']
+  final_fc_weights = tf.compat.v1.get_variable(
+      name='final_fc_weights',
+      initializer=tf.compat.v1.truncated_normal_initializer(stddev=0.01),
+      shape=[first_dropout_element_count, label_count])
+  final_fc_bias = tf.compat.v1.get_variable(
+      name='final_fc_bias',
+      initializer=tf.compat.v1.zeros_initializer,
+      shape=[label_count])
+  final_fc = (
+      tf.matmul(flattened_first_dropout, final_fc_weights) + final_fc_bias)  # matmul multiplies Flat first dropout * final weights
+  if is_training:
+    return final_fc, dropout_prob
+  else:
+    return final_fc
 
 
 def create_ds_cnn_model(fingerprint_input, model_settings, model_size_info, is_training):
