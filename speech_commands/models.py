9a709665715e2e692e52f76886a0d539fd7c18d1
martin v <villasen@unlv.nevada.edu>
2021-01-23 00:21:48 -0800

    Fixing indentation error

--- speech_commands/models.py
+++ speech_commands/models.py
@@ -1150,6 +1150,30 @@ def create_martin_urban_conv_model(fingerprint_input, model_settings,
   else:
     first_dropout = first_relu
 
+    
+  first_dropout_shape = first_dropout.get_shape()
+  first_dropout_output_width = first_dropout_shape[2]
+  first_dropout_output_height = first_dropout_shape[1]
+  first_dropout_element_count = int(
+      first_dropout_output_width * first_dropout_output_height *
+      first_filter_count)
+  flattened_first_dropout = tf.reshape(first_dropout,
+                                       [-1, first_dropout_element_count])
+  label_count = model_settings['label_count']
+  final_fc_weights = tf.compat.v1.get_variable(
+      name='final_fc_weights',
+      initializer=tf.compat.v1.truncated_normal_initializer(stddev=0.01),
+      shape=[first_dropout_element_count, label_count])
+  final_fc_bias = tf.compat.v1.get_variable(
+      name='final_fc_bias',
+      initializer=tf.compat.v1.zeros_initializer,
+      shape=[label_count])
+  final_fc = (
+      tf.matmul(flattened_first_dropout, final_fc_weights) + final_fc_bias)
+  if is_training:
+    return final_fc, dropout_prob
+  else:
+    return final_fc
 
 
 '''
@@ -1178,29 +1202,7 @@ def create_martin_urban_conv_model(fingerprint_input, model_settings,
   
   first_relu = tf.nn.relu(first_conv)
 '''
-  first_dropout_shape = first_dropout.get_shape()
-  first_dropout_output_width = first_dropout_shape[2]
-  first_dropout_output_height = first_dropout_shape[1]
-  first_dropout_element_count = int(
-      first_dropout_output_width * first_dropout_output_height *
-      first_filter_count)
-  flattened_first_dropout = tf.reshape(first_dropout,
-                                       [-1, first_dropout_element_count])
-  label_count = model_settings['label_count']
-  final_fc_weights = tf.compat.v1.get_variable(
-      name='final_fc_weights',
-      initializer=tf.compat.v1.truncated_normal_initializer(stddev=0.01),
-      shape=[first_dropout_element_count, label_count])
-  final_fc_bias = tf.compat.v1.get_variable(
-      name='final_fc_bias',
-      initializer=tf.compat.v1.zeros_initializer,
-      shape=[label_count])
-  final_fc = (
-      tf.matmul(flattened_first_dropout, final_fc_weights) + final_fc_bias)
-  if is_training:
-    return final_fc, dropout_prob
-  else:
-    return final_fc
+
 
 
 
